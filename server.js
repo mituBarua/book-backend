34e27c94202cb3546a6e2300a28a982c25cc1569
mituBarua <mitubarua766@gmail.com>
2023-07-31 20:43:47 +0600

    initial commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://bookStore:nvycFptQc4aSHpx3@cluster0.fsd1c.mongodb.net/`;
+const uri = `mongodb+srv://pcBuilder:Ude317x0vqoXcUJD@cluster0.fsd1c.mongodb.net/`;
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -19,69 +19,38 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("bookStore");
-    const bookCollection = db.collection("book");
+    const db = client.db("pcBuilder");
+    const pcBuilder = db.collection("featuredProduct");
 
-    app.get("/books", async (req, res) => {
-      const cursor = bookCollection.find({});
-      const book = await cursor.toArray();
-      res.send({ status: true, data: book });
+    app.get("/featured-product", async (req, res) => {
+      const cursor = pcBuilder.find({});
+      const featuredProduct = await cursor.toArray();
+      res.send({ status: true, data: featuredProduct });
     });
-
-    app.post("/book", async (req, res) => {
-      const book = req.body;
-
-      const result = await bookCollection.insertOne(book);
-
-      res.send(result);
-    });
-
-    app.get("/book/:id", async (req, res) => {
+    app.get("/featured-product/:id", async (req, res) => {
       const id = req.params.id;
 
-      const result = await bookCollection.findOne({ _id: ObjectId(id) });
+      const result = await pcBuilder.findOne({ _id: ObjectId(id) });
 
       res.send(result);
     });
-    app.put("/book/:id", async (req, res) => {
-      const id = req.params.id;
-      const updatedBook = req.body;
-    
+  } finally {
+  }
+  try {
+    const db = client.db("pcBuilder");
+    const pcBuilder2 = db.collection("featuredCategory");
 
-      const result = await bookCollection.updateOne(
-        { _id: ObjectId(id) },
-        { $set: updatedBook }
-      );
-
-      res.send(result);
+    app.get("/featured-category", async (req, res) => {
+      const cursor = pcBuilder2.find({});
+      const featuredCategory = await cursor.toArray();
+      res.send({ status: true, data: featuredCategory });
     });
-
-    app.delete("/book/:id", async (req, res) => {
+    app.get("/featured-category/:id", async (req, res) => {
       const id = req.params.id;
 
-      const result = await bookCollection.deleteOne({ _id: ObjectId(id) });
+      const result = await pcBuilder2.findOne({ _id: ObjectId(id) });
 
       res.send(result);
-    });
-
-    app.post("/user", async (req, res) => {
-      const user = req.body;
-
-      const result = await userCollection.insertOne(user);
-
-      res.send(result);
-    });
-
-    app.get("/user/:email", async (req, res) => {
-      const email = req.params.email;
-
-      const result = await userCollection.findOne({ email });
-
-      if (result?.email) {
-        return res.send({ status: true, data: result });
-      }
-
-      res.send({ status: false });
     });
   } finally {
   }
